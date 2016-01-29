404f16544d7556661d410d5e8378c9239ccdacbd
gamejam <gamejam>
2016-01-29 23:27:44 +0000

    Added Resource manager script

--- Assets/Scripts/CameraMoveController.cs
+++ Assets/Scripts/CameraMoveController.cs
@@ -4,29 +4,38 @@ using System.Collections;
 public class CameraMoveController : MonoBehaviour {
 
     private Transform myTransform;
+    private Camera myCamera;
 
 	// Use this for initialization
 	void Start () {
         myTransform = GetComponent<Transform>();
-	}
+        myCamera = GetComponent<Camera>();
+    }
 	
 	// Update is called once per frame
 	void Update () {
 	    if (Input.GetKeyDown(KeyCode.DownArrow))
         {
-            myTransform.position = new Vector3(myTransform.position.x, myTransform.position.y - 2, 0);
+            myTransform.position = new Vector3(myTransform.position.x, myTransform.position.y - 2, -10);
         }
         else if(Input.GetKeyDown(KeyCode.RightArrow))
         {
-            myTransform.position = new Vector3(myTransform.position.x + 2, myTransform.position.y, 0);
+            myTransform.position = new Vector3(myTransform.position.x + 2, myTransform.position.y, -10);
         }
         else if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
-            myTransform.position = new Vector3(myTransform.position.x - 2, myTransform.position.y, 0);
+            myTransform.position = new Vector3(myTransform.position.x - 2, myTransform.position.y, -10);
         }
         else if (Input.GetKeyDown(KeyCode.UpArrow))
         {
-            myTransform.position = new Vector3(myTransform.position.x, myTransform.position.y + 2, 0);
+            myTransform.position = new Vector3(myTransform.position.x, myTransform.position.y + 2, -10);
         }
+    }
+
+    public Vector2 AbsolutePositionUnerMouse()
+    {
+        Vector3 checkVector = myCamera.ScreenToWorldPoint(Input.mousePosition);
+
+        return new Vector2(Mathf.Floor(checkVector.x), Mathf.FloorToInt(checkVector.y));
     }
 }
---
+++ Assets/Scripts/ResourceManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ResourceManager : MonoBehaviour {
+
+    private int _gold;
+    public int Gold
+    {
+        get { return _gold; }
+        set { _gold = value; }
+    }
+    
+    private int _souls;
+    public int Souls
+    {
+        get { return _souls; }
+        set { _souls = value; }
+    }
+
+    private float _renown;
+    public float Renown
+    {
+        get { return _renown; }
+        set { _renown = value; }
+    }
+
+    
+}
