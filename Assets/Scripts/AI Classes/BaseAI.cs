318d3803198ee5dcce1b19e89cd6d08a058790e4
Nairu <Carniall@googlemail.com>
2016-01-30 02:57:21 +0000

    Repush

---
+++ Assets/Scripts/AI Classes/BaseAI.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BaseAI : MonoBehaviour {
+
+    private int _health;
+    public int Health
+    {
+        get { return Health; }
+    }
+
+    public void DealDamage(int damageToDeal)
+    {
+        _health -= damageToDeal;
+        if (_health < 0)
+            Die();
+    }
+
+    public void HealDamage(int amountToHeal)
+    {
+        _health += amountToHeal;
+    }
+
+    private void Die()
+    {
+
+    }
+}
