3b3dc5efd8d339a9e7455e3965e764255c9eeb9e
gamejam <gamejam>
2016-01-30 00:22:05 +0000

    Added dirt and tunnel prefab, and worked on MapManager

---
+++ Assets/Scripts/MapManager.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MapManager : MonoBehaviour {
+
+    public int numTilesY;
+    public int numTilesX;
+
+
+	// Use this for initialization
+	void Start () {
+        SpawnTiles();
+
+
+	}
+
+    void SpawnTiles()
+    {
+        numTilesY = Mathf.CeilToInt(Camera.main.orthographicSize) * 2;
+        numTilesX = Mathf.CeilToInt(Screen.width / Screen.height * numTilesY) * 2;
+
+        int x = Mathf.FloorToInt(Camera.main.transform.position.x - numTilesX / 2);
+
+        for (; x < Camera.main.transform.position.x + Mathf.CeilToInt(numTilesX / 2) + 1; x = x+3)
+        {
+            int y = Mathf.FloorToInt(Camera.main.transform.position.y - numTilesY / 2);
+            for (; y < Mathf.CeilToInt(numTilesY / 2) + 1; y = y+2)
+            {
+                if (y > 0)
+                    break;
+
+                string tileName = "Tile_" + x + "_" + y;
+                Vector2 tilePos = new Vector2(x, y);
+                CreateTile(tilePos, tileName, "Dirt");
+            }
+        }
+    }
+
+    void CreateTile(Vector2 pos, string name, string type)
+    {
+        GameObject prefab = Resources.Load<GameObject>(type);
+        // check type exists iin prefabs
+        if (prefab == null)
+        {
+            Debug.LogError("No prefab found: " + type);
+        }
+
+        GameObject go = (GameObject)GameObject.Instantiate(prefab);
+        go.name = name;
+        go.transform.position = pos;
+        go.transform.parent = this.transform;
+
+        Tile tile = go.GetComponent<Tile>();
+        tile.Pos = pos;
+        tile.TileName = name;
+
+    }
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/Tile.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tile : MonoBehaviour {
+
+    public string TileName;
+
+    public Vector2 Pos;
+    public string TileType;
+
+    MapManager map;
+
+	// Use this for initialization
+	void Start () {
+        map = GameObject.FindObjectOfType<MapManager>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
