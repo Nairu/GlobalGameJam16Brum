a36acda538aafc89a98604611977bd9c424c3422
Twig <adelaney2011@hotmail.co.uk>
2016-01-31 08:07:03 +0000

    Quests Update 0.4

--- Assets/Scripts/Quest System/Quest Manager.cs
+++ Assets/Scripts/Quest System/Quest Manager.cs
@@ -5,9 +5,10 @@ using System.Linq;
 
 enum questType { buildQuest, followerQuest, lootQuest, summonQuest}
 
-public class QuestManager {
+public class QuestManager
+{
 
-    private Quest[] activeQuests;
+    private List<Quest> activeQuests;
     private int currentCultists;
     private int currentDemons;
     private int currentGold;
@@ -18,13 +19,52 @@ public class QuestManager {
     public QuestManager(int currentCultists, int currentDemons, int currentGold, int currentRenown)
     {
         this.currentCultists = currentCultists; this.currentDemons = currentDemons; this.currentGold = currentGold; this.currentRenown = currentRenown;
-        activeQuests = new Quest[5]; // The number here represents the maximum amount of active quests a player can have.
+        activeQuests = new List<Quest>(5); // The number here represents the maximum amount of active quests a player can have.
     }
 
 
-    public void startQuestManager()
+    public void runQuestManager(int currentCultists, int currentDemons, int currentGold, int currentRenown)
     {
+        this.currentCultists = currentCultists; this.currentDemons = currentDemons; this.currentGold = currentGold; this.currentRenown = currentRenown;
+        int generationChance = UnityEngine.Random.Range(1, 10801);
+        if(generationChance == 10800)
+        {
+            activeQuests.Add(genRandQuest());
+        }
 
+        foreach (Quest q in activeQuests)
+        {
+            int valueToPass = 1;
+            switch (q.GetType().ToString())
+            {
+                case "BuildQuest":
+                    //valueToPass = currentNumberOfRoomsOfType -.-
+                    break;
+                case "LootQuest":
+                    valueToPass = currentGold;
+                    break;
+                case "GetFollowerQuest":
+                    valueToPass = currentCultists;
+                    break;
+                case "SummonDemonQuest":
+                    valueToPass = currentDemons;
+                    break;
+                default:
+                    throw new System.Exception("Wrong type of quest");
+            }
+
+            if (q.QuestFailed())
+            {
+                activeQuests.Remove(q);
+                // Bring up quest failed diaglog?
+            }
+            else if (q.QuestSucceeded(valueToPass))
+            {
+                activeQuests.Remove(q);
+                // Quest Succeeded dialog
+                // Give player reward for completing quest
+            }
+        }       
     }
 
     private Quest genRandQuest()
@@ -64,4 +104,5 @@ public class QuestManager {
     {
         return activeQuests[questNum].getQuestText();
     }
+
 }
