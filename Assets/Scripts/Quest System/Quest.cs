51420d4441f3a12c6c1a2ed7a59aa81f77fdfaac
Twig <adelaney2011@hotmail.co.uk>
2016-01-31 00:04:12 +0000

    Quests 0.1

---
+++ Assets/Scripts/Quest System/Quest.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Diagnostics;
+
+public abstract class Quest {
+
+    protected Stopwatch watch;
+    protected string questText;
+    protected string[] listOfQuests;
+
+    protected int timeLimit;
+
+    protected int rewardGold;
+    protected int rewardSouls;
+    protected int rewardRenown;
+
+    protected Quest(int timeLimit)
+    {
+        watch = new Stopwatch();
+        this.timeLimit = timeLimit;
+    }
+
+    protected void StartQuest()
+    {
+        watch.Start();
+    }
+
+    public bool QuestFailed()
+    {
+        int elapsedTime = Mathf.RoundToInt(watch.ElapsedMilliseconds / 1000);
+        if( elapsedTime == timeLimit)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public abstract bool QuestSucceeded(int i);
+    
+    public int getGoldReward()
+    {
+        return rewardGold;
+    }
+
+    public int getRenownReward()
+    {
+        return rewardRenown;
+    }
+
+    public int getSoulsReward()
+    {
+        return rewardSouls;
+    }
+
+
+
+    // Use this for initialization
+    void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/Quest System/Summon Demon Quest.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class SummonDemonQuest : Quest {
+
+
+    public SummonDemonQuest(int timeLimit) : base(timeLimit){
+
+    }
+
+    public override bool QuestSucceeded(int i)
+    {
+        throw new NotImplementedException();
+    }
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
